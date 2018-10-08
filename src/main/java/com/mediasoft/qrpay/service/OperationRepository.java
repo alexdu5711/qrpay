69f4c3ab21584c5e8cbf117dbf3163041fe47c47
alexdu5711 <talent5711>
2018-10-08 18:34:33 +0000

    Operation

--- src/main/java/com/mediasoft/qrpay/service/OperationRepository.java
+++ src/main/java/com/mediasoft/qrpay/service/OperationRepository.java
@@ -1,11 +1,6 @@
 package com.mediasoft.qrpay.service;
-
-import com.mediasoft.qrpay.entities.Annonces;
 import com.mediasoft.qrpay.entities.Operation;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface OperationRepository extends JpaRepository<Operation,Long> {
 
